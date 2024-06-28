58c743546418d0a6454a3a137d08b79cf3ff05f1
Nitish-Kumar-2 <meritnitishkumar@gmail.com>
2024-06-29 01:34:59 +0530

    setup env :

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -8,9 +8,7 @@ export const isAuthenticated = catchAsyncErrors(async (req, res, next) => {
   if (!token) {
     return next(new ErrorHandler("User Not Authorized", 401));
   }
-  const decoded = jwt.verify(token, "nitish");
-  // console.log("🚀 ~ isAuthenticated ~ decoded:", decoded)
-
+  const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
   req.user = await User.findById(decoded.id);
 
   next();
